706b1889dd56c5d02c3254d36221c55a276ddaef
Santiago <santirensonnet@gmail.com>
2022-12-29 15:22:35 -0700

    Item type error fix

--- src/components/cart/types.ts
+++ src/components/cart/types.ts
@@ -3,7 +3,8 @@ type Item = {
   name: string;
   price: number;
   imageUrl: string;
-  imageYOffset: string; //Y offset from centered position
+  imageXOffset?: string;
+  imageYOffset?: string; //Y offset from centered position
   qty: number;
 };
 export type { Item };
